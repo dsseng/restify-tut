c3e1301857314365b96b98052c3c2d0d41a96810
Dmitriy Sharshakov aka. sh7dm <sh7dm@outlook.com>
2018-02-15 22:08:05 +0300

    Added JWT "auth"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import restify from 'restify'
+import jwt from 'jsonwebtoken'
 
 function respond (req, res, next) {
   res.send('hello ' + req.params.name)
@@ -6,6 +7,32 @@ function respond (req, res, next) {
 }
 
 var server = restify.createServer()
+
+server.use(restify.plugins.queryParser({
+  mapParams: true
+}));
+server.use(restify.plugins.bodyParser({
+  mapParams: true
+}));
+
+server.post('/login', (req, res, next) => {
+  res.send({
+    token: jwt.sign({ data: 'asd' }, 'asd', { expiresIn: '2h' })
+  })
+  next()
+})
+server.post('/secure', (req, res, next) => {
+  jwt.verify(req.body.token, 'asd', (err, payload) => {
+    if (!err) {
+      res.send({
+        payload: payload
+      })
+    } else {
+      return next(new Error('Token invalid!'));
+    }
+    next()
+  })
+})
 server.get('/hello/:name', respond)
 server.head('/hello/:name', respond)
 
